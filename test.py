d661bdab5acd68ec86e763d734ec8f268d6bf707
Anton Panas <poster.instant@gmail.com>
2019-01-10 23:43:10 +0200

    refactor code

--- test.py
+++ test.py
@@ -17,17 +17,21 @@ class Test(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_test(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        wd.find_element_by_link_text("Адреса").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Вийти").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         wd.find_element_by_link_text("Групи").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
@@ -41,9 +45,21 @@ class Test(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test footer")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Вийти").click()
-        self.assertTrue(success)
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("Адреса").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost")
 
     def tearDown(self):
         self.wd.close()
