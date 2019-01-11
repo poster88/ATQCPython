77e33f311af13a00b4de2291d84d249d92e4a923
Anton Panas <poster.instant@gmail.com>
2019-01-11 17:17:58 +0200

    refactor code

--- test.py
+++ test.py
@@ -16,50 +16,59 @@ class Test(unittest.TestCase):
         self.wd = webdriver.Firefox(executable_path="C:\\webdrivers\\geckodriver.exe")
         self.wd.implicitly_wait(60)
 
-    def test_test(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_groups_page(wd)
-        self.create_group(wd)
-        self.return_to_group_page(wd)
-        self.logout(wd)
+    def open_home_page(self, wd):
+        wd.get("http://localhost")
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Вийти").click()
-
-    def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd):
-        wd.find_element_by_link_text("Групи").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test header")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test footer")
-        wd.find_element_by_name("submit").click()
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("Адреса").click()
 
-    def login(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+    def create_group(self, wd, name, header, footer):
+        wd.find_element_by_link_text("Групи").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("submit").click()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost")
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Вийти").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="test", header="test header", footer="test footer")
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.close()
