92a44e90503dae2b516935be2737099af3cf9737
Anton Panas <poster.instant@gmail.com>
2019-01-11 23:43:15 +0200

    create test for add contact

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 
+from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
@@ -12,6 +13,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
---
+++ test_add_contact.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.contact import Contact
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Second uncle", middlename="Bob", email="uncle.bob@gmail.com"))
+    app.session.logout()
+
+
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", middlename="", email=""))
+    app.session.logout()
