e041bef9cf2bd643e010cd47e4d8cc91e0a4b681
Anton Panas <poster.instant@gmail.com>
2019-01-11 19:43:15 +0200

    clean code

--- test_add_group.py
+++ test_add_group.py
@@ -1,13 +1,13 @@
-import unittest
+# -*- coding: utf-8 -*-
 import pytest
-from application import Application
 from group import Group
+from application import Application
 
 
-@pytest.fixture()
+@pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy())
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
@@ -21,7 +21,3 @@ def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
-
-if __name__ == '__main__':
-    unittest.main()
