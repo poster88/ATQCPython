3f90a99bed9b07906c241f7a3a94af9055508254
Anton Panas <poster.instant@gmail.com>
2019-01-13 03:22:25 +0200

    move open_home_page to application class

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope="session", autouse=False)
+@pytest.fixture(scope="session", autouse=True)
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
