8f2699eb22ea1f34fd9df46f6416ca1c04f23d22
Anton Panas <poster.instant@gmail.com>
2019-01-14 22:14:19 +0200

    separate fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Second uncle", middlename="Bob", email="uncle.bob@gmail.com"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", email=""))
-    app.session.logout()
